bf5997174a1c241fa8d7eb826893acf319544bd3
Salokya Kumar <kumar.salokya007@gmail.com>
2021-09-05 13:39:44 +0530

    added fonts

--- src/App.js
+++ src/App.js
@@ -18,4 +18,48 @@ class App extends Component{
     }
 }
 
+
+//importing extra fonts
+let Font = Quill.import('formats/font');
+Font.whitelist = ['times-new-roman', 'arial'];
+Quill.register(Font, true)
+
+App.modules = {
+    toolbar: [
+        [{ header: "1" }, { header: "2" }, { font: [' ', 'times-new-roman', 'arial'] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+        [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" }
+        ],
+        ["link", "image", "video"],
+        ["clean"]
+    ],
+    clipboard: {
+        matchVisual: false
+    }
+};
+
+App.formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+    "code-block"
+];
+
+
 export default App;
